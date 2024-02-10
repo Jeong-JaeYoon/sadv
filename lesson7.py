c63eb02b1a976ef7fc03ebb253e6fb27e4fb7ba3
unknown <jj950310@naver.com>
2024-02-10 19:32:11 +0900

    lesson 7

--- lesson7.py
+++ lesson7.py
@@ -18,3 +18,23 @@ elif m == 2:
 elif m == 3:
     for i in range(n-1, -1, -1):
         print(' '*(i) + '*'*(n-i) + '*'*(n-i-1))
+
+## 해설
+
+n, m = map(int, input().split())
+
+if 1<=n<=100 and 1<=m<=3:
+    if type==1:
+        for i in range(1, n+1):
+            print("*"*i)
+
+    elif type==2:
+        for i in range(n, 0, -1):
+            print("*"*i)
+
+    else:
+        for i in range(n):
+            print(' '*(n-i-1) + '*'*(2*i+1))
+
+else:
+    print('INPUT ERROR!')
