b2306fbe728585258ca6df2c5e057ba45b587dff
unknown <jj950310@naver.com>
2024-02-20 00:33:55 +0900

    lesson 12

--- lesson12.py
+++ lesson12.py
@@ -18,8 +18,37 @@ if m == 1:
         print(*a)
 
 elif m == 2:
-    for i in range(n-1, -1, -1):
-        print(*A[i])
+    for i, a in enumerate(A[::-1]):
+        print(' '*i, end='')
+        print(*a)
 
 else:
-    pass
+    for i in range(n):
+        for j in range(i+1):
+            r, c = n-j-1, n-i-1
+            print(A[r][c], end=' ')
+        print()
+
+
+## 해설
+n, m = map(int, input().split())
+A = [[1]*i for i in range(1, n+1)]
+for i in range(2, n):
+    for j in range(1, i):
+        A[i][j] = A[i-1][j-1]+A[i-1][j]
+
+if m == 1:
+    for a in A:
+        print(*a)
+
+if m == 2:
+    for i, a in enumerate(A[::-1]):
+        print(' '*i, end='')
+        print(*a)
+
+if m == 3:
+    for i in range(n):
+        for j in range(i+1):
+            r, c = n-j-1, n-i-1
+            print(A[r][c], end = ' ')
+        print()
