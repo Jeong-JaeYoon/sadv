43df7eeef36a8befdcf41563e4e00c009e7fcd53
unknown <jj950310@naver.com>
2024-03-18 00:04:20 +0900

    lesson 20

--- lesson20.py
+++ lesson20.py
@@ -1,5 +1,11 @@
 import sys
 sys.stdin = open('input.txt')
 
-n = int(input())
+A = [[0]*100 for _ in range(100)]
 
+for _ in range(int(input())):
+    X, Y = map(int, input().split())
+    for x in range(X, X+10):
+        A[x][Y:Y+10] = [1]*10
+
+print(sum(sum(a) for a in A))
